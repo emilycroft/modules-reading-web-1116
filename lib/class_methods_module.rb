b815a9d3efd441b31f4320d55ff3dc47ffc954d4
emilycroft <emilyhcroft@gmail.com>
2016-12-03 17:03:30 -0500

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,9 @@
+# 
+# require_relative './dance_module.rb'
+# require_relative './fancy-dance.rb'
+
+module MetaDancing
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
